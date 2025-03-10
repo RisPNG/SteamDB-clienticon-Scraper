3a7961da8cf8451c66c0c2ef457cbc461bdc3b33
RisPNG <ris@poppybit.com>
2025-03-10 15:40:18 +0800

    Refactor variable names for clarity: update Steam Web API key and Steam ID variables

--- get_owned_app_ids.py
+++ get_owned_app_ids.py
@@ -4,8 +4,8 @@ import json
 # Replace with your Steam Web API key and Steam ID
 # Steam Web API Key: https://steamcommunity.com/dev/apikey
 # Steam64 ID / steamID64: https://steamid.xyz/
-STEAM_WEB_API_KEY = "Steam Web API Key"
-STEAM_ID = "76561198028632468"
+STEAM_WEB_API_KEY = "Steam_Web_API_Key"
+STEAM64_ID = "76561198028632468"
 
 # Steam API endpoint to get owned games
 STEAM_API_ENDPOINT = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
@@ -13,7 +13,7 @@ STEAM_API_ENDPOINT = "https://api.steampowered.com/IPlayerService/GetOwnedGames/
 # Parameters for the API request
 params = {
     "key": STEAM_WEB_API_KEY,
-    "steamid": STEAM_ID,
+    "steamid": STEAM64_ID,
     "include_appinfo": "1",  # Include app info
     "include_played_free_games": "1",  # Include free games
     "format": "json"
